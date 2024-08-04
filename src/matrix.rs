a68f384b549ddc5c835429901d3fda237ded13be
jyy <1305081045@qq.com>
2024-08-04 12:54:53 +0800

    refactor: make matrix multiply to be concurreny with threads

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,14 +1,46 @@
+use crate::{dot_product, Vector};
 use anyhow::anyhow;
 use std::{
     fmt,
     ops::{Add, AddAssign, Mul},
+    sync::mpsc,
+    thread,
 };
+
+const NUM_THREADS: usize = 4;
 
 // [[1, 2], [1, 2], [1, 2]] => [1, 2, 1, 2, 1, 2]
 pub struct Matrix<T> {
     data: Vec<T>,
     row: usize,
     col: usize,
+}
+
+pub struct MsgInput<T> {
+    idx: usize,
+    row: Vector<T>,
+    col: Vector<T>,
+}
+
+pub struct MsgOutput<T> {
+    idx: usize,
+    value: T,
+}
+
+pub struct Msg<T> {
+    input: MsgInput<T>,
+    sender: oneshot::Sender<MsgOutput<T>>,
+}
+
+impl<T> Mul for Matrix<T>
+where
+    T: Mul<Output = T> + Copy + Default + Add<Output = T> + AddAssign + Send + 'static,
+{
+    type Output = anyhow::Result<Matrix<T>>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        multiply::<T>(&self, &rhs)
+    }
 }
 
 impl<T: fmt::Debug> Matrix<T> {
@@ -54,18 +86,60 @@ where
 
 pub fn multiply<T>(a: &Matrix<T>, b: &Matrix<T>) -> anyhow::Result<Matrix<T>>
 where
-    T: Copy + Default + Add<Output = T> + AddAssign + Mul<Output = T>,
+    T: Copy + Default + Add<Output = T> + AddAssign + Mul<Output = T> + Send + 'static,
 {
     if a.col != b.row {
         return Err(anyhow!("Matrix multiply error: a.col != b.row"));
     }
-    let mut data = vec![T::default(); a.row * b.col];
+    let senders = (0..NUM_THREADS)
+        .map(|_| {
+            let (tx, rx) = mpsc::channel::<Msg<T>>();
+            thread::spawn(move || {
+                for msg in rx {
+                    let value = dot_product(msg.input.row, msg.input.col)?;
+                    if let Err(e) = msg.sender.send(MsgOutput {
+                        idx: msg.input.idx,
+                        value,
+                    }) {
+                        eprintln!("Send error: {:?}", e);
+                    }
+                }
+                Ok::<_, anyhow::Error>(())
+            });
+
+            tx
+        })
+        .collect::<Vec<_>>();
+
+    let matrix_len = a.row * b.col;
+    let mut data = vec![T::default(); matrix_len];
+    let mut receivers = Vec::with_capacity(matrix_len);
+
     for i in 0..a.row {
         for j in 0..b.col {
-            for k in 0..a.col {
-                data[i * b.col + j] += a.data[i * a.col + k] * b.data[k * b.col + j];
+            let row = Vector::new(&a.data[i * a.col..(i + 1) * a.col]);
+            let col_data = b.data[j..]
+                .iter()
+                .step_by(b.col)
+                .copied()
+                .collect::<Vec<_>>();
+
+            let col = Vector::new(col_data);
+            let idx = i * b.col + j;
+            let input = MsgInput { idx, row, col };
+            let (tx, rx) = oneshot::channel();
+            let msg = Msg { input, sender: tx };
+            if let Err(e) = senders[idx % NUM_THREADS].send(msg) {
+                eprintln!("Send error: {:?}", e);
             }
+            receivers.push(rx);
         }
+    }
+
+    // map/reduce: reduce phase
+    for rx in receivers {
+        let output = rx.recv()?;
+        data[output.idx] = output.value;
     }
     Ok(Matrix {
         data,
---
+++ src/vector.rs
@@ -0,0 +1,37 @@
+use std::ops::{Add, AddAssign, Deref, Mul};
+
+use anyhow::{anyhow, Result};
+
+pub struct Vector<T> {
+    data: Vec<T>,
+}
+
+pub fn dot_product<T>(a: Vector<T>, b: Vector<T>) -> Result<T>
+where
+    T: Copy + Default + Add<Output = T> + AddAssign + Mul<Output = T>,
+{
+    if a.len() != b.len() {
+        return Err(anyhow!("Dot product error: a.len != b.len"));
+    }
+
+    let mut sum = T::default();
+    for i in 0..a.len() {
+        sum += a[i] * b[i]
+    }
+
+    Ok(sum)
+}
+
+impl<T> Deref for Vector<T> {
+    type Target = Vec<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+}
+
+impl<T> Vector<T> {
+    pub fn new(data: impl Into<Vec<T>>) -> Self {
+        Self { data: data.into() }
+    }
+}
